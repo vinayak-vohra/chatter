60ecf680f0f47ffcc42be49d1dddb37189387d55
Vinayak Vohra <vn26vhr@gmail.com>
2024-07-31 00:53:16 +0530

    add: shadcn integration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,22 @@
 import { light } from "./src/themes";
-
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         Playwrite: ["'Playwrite ES Deco'"],
@@ -15,9 +27,21 @@ export default {
           "0%": { transform: "scale(1)" },
           "100%": { transform: "scale(0)" },
         },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   daisyui: { themes: [{ light }] },
-  plugins: [require("daisyui")],
+  plugins: [require("tailwindcss-animate"), require("daisyui")],
 };
